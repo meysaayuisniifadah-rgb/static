b78fc66dd3b954dcf68a47976554febc53c37245
meyy <meysaayuisniifadah@gmail.com>
2026-04-22 16:40:41 +0700

    after

--- Tugas/Counter_pengunjung.php
+++ Tugas/Counter_pengunjung.php
@@ -5,6 +5,10 @@ class Pengunjung {
     public function __construct() {
         self::$jumlah++;
     }
+
+    public static function reset() {
+        self::$jumlah = 0;
+    }
 }
 
 
@@ -14,5 +18,7 @@ $p3 = new Pengunjung();
 $p4 = new Pengunjung();
 $p5 = new Pengunjung();
 
-echo "Jumlah Pengunjung: " . Pengunjung::$jumlah;
+Pengunjung::reset();
+
+echo "Jumlah Pengunjung setelah reset: " . Pengunjung::$jumlah;
 ?>
